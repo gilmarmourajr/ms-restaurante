fa026d33093930246acac112cc02713a260c49b4
gilmarmourajr <juzinho-gilmar@hotmail.com>
2026-03-11 11:45:08 -0300

    Start restauranteController class

--- src/main/java/com/github/gilmarmourajr/ms_restaurante/controller/RestauranteController.java
+++ src/main/java/com/github/gilmarmourajr/ms_restaurante/controller/RestauranteController.java
@@ -1,9 +1,14 @@
 package com.github.gilmarmourajr.ms_restaurante.controller;
 
+import com.github.gilmarmourajr.ms_restaurante.dto.RestauranteDTO;
 import com.github.gilmarmourajr.ms_restaurante.service.RestauranteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/restaurantes")
@@ -12,4 +17,11 @@ public class RestauranteController {
 
     @Autowired
     private RestauranteService restauranteService;
+
+    @GetMapping
+    public ResponseEntity<List<RestauranteDTO>> getAllRestaurantes(){
+        List<RestauranteDTO> restaurantes = restauranteService.findAllRestaurantes();
+
+        return ResponseEntity.ok(restaurantes);
+    }
 }
