a661523f8c25085a37c132f2cb0ac19308215ef7
gilmarmourajr <juzinho-gilmar@hotmail.com>
2026-03-18 02:54:03 -0300

    Finish controller classes

--- src/main/java/com/github/gilmarmourajr/ms_restaurante/controller/ReservaController.java
+++ src/main/java/com/github/gilmarmourajr/ms_restaurante/controller/ReservaController.java
@@ -1,9 +1,16 @@
 package com.github.gilmarmourajr.ms_restaurante.controller;
 
+import com.github.gilmarmourajr.ms_restaurante.dto.ReservaDTO;
+import com.github.gilmarmourajr.ms_restaurante.entities.Reserva;
 import com.github.gilmarmourajr.ms_restaurante.service.ReservaService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping("/reservas")
@@ -11,4 +18,45 @@ public class ReservaController {
 
     @Autowired
     ReservaService reservaService;
+
+    @GetMapping
+    public ResponseEntity<List<ReservaDTO>> getAllReservas(){
+        List<ReservaDTO> list = reservaService.findAllReservas();
+
+        return ResponseEntity.ok(list);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ReservaDTO> getReservaById(@PathVariable Long id){
+        ReservaDTO reservaDTO = reservaService.findReservaById(id);
+
+        return ResponseEntity.ok(reservaDTO);
+    }
+
+    @PostMapping
+    public ResponseEntity<ReservaDTO> createReserva(@RequestBody @Valid ReservaDTO reservaDTO) {
+        reservaDTO = reservaService.saveReserva(reservaDTO);
+
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequestUri()
+                .path("/{id}")
+                .buildAndExpand(reservaDTO.getId())
+                .toUri();
+
+        return ResponseEntity.created(uri).body(reservaDTO);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ReservaDTO> updateReserva(@PathVariable Long id, @RequestBody @Valid ReservaDTO reservaDTO){
+        reservaDTO = reservaService.updateReserva(id, reservaDTO);
+
+        return ResponseEntity.ok(reservaDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProduto(@PathVariable Long id){
+        reservaService.deleteReservaById(id);
+
+        return ResponseEntity.noContent().build();
+    }
 }
--- src/main/java/com/github/gilmarmourajr/ms_restaurante/controller/RestauranteController.java
+++ src/main/java/com/github/gilmarmourajr/ms_restaurante/controller/RestauranteController.java
@@ -2,12 +2,13 @@ package com.github.gilmarmourajr.ms_restaurante.controller;
 
 import com.github.gilmarmourajr.ms_restaurante.dto.RestauranteDTO;
 import com.github.gilmarmourajr.ms_restaurante.service.RestauranteService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -23,5 +24,37 @@ public class RestauranteController {
         List<RestauranteDTO> restaurantes = restauranteService.findAllRestaurantes();
 
         return ResponseEntity.ok(restaurantes);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<RestauranteDTO> getRestauranteById(@PathVariable Long id){
+        RestauranteDTO restauranteDTO = restauranteService.findRestauranteById(id);
+
+        return ResponseEntity.ok(restauranteDTO);
+    }
+
+    @PostMapping
+    public ResponseEntity<RestauranteDTO> createRestaurante(@Valid @RequestBody RestauranteDTO restauranteDTO){
+        restauranteDTO = restauranteService.saveRestaurante(restauranteDTO);
+
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequestUri()
+                .path("/{id}")
+                .buildAndExpand(restauranteDTO.getId())
+                .toUri();
+
+        return ResponseEntity.created(uri).body(restauranteDTO);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<RestauranteDTO> updateRestaurante(@PathVariable Long id, @RequestBody @Valid RestauranteDTO restauranteDTO){
+        restauranteDTO = restauranteService.updateRestaurante(id, restauranteDTO);
+        return ResponseEntity.ok(restauranteDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCategoria(@PathVariable Long id){
+        restauranteService.deleteRestauranteById(id);
+        return ResponseEntity.noContent().build();
     }
 }
